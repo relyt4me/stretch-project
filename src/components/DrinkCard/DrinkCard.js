4b1381ac3d880fcd3413c1cf34854ecae4432ef4
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-09 17:25:51 -0600

    Add UI for DrinkCard component

--- src/components/DrinkCard/DrinkCard.js
+++ src/components/DrinkCard/DrinkCard.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './DrinkCard.css'
 
 const DrinkCard = ({id, name, image, alcoholContent}) => {
-
+  return (
+    <section style={{backgroundImage: `url(${image})`}}>
+      <p>{alcoholContent}</p>
+      <h3>{name}</h3>
+    </section>
+  )
 }
 
 export default DrinkCard
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -4,19 +4,23 @@ import './Results.css'
 import results from './testdata'
 
 const Results = () => {
+  console.log(results)
   const resultsList = results.map(result => {
-    return <DrinkCard 
-      id={result.idDrink}
-      name={result.strDrink}
-      image={result.strDrinkThumb}
-      alcoholContent={result.strAlcoholic}
-    />
+    return (
+      <DrinkCard 
+        key={result.idDrink}
+        id={result.idDrink}
+        name={result.strDrink}
+        image={result.strDrinkThumb}
+        alcoholContent={result.strAlcoholic}
+      />
+    )
   })
 
   return (
       <section>
         {resultsList.length > 0 &&
-          {resultsList}
+          resultsList
         }
         {resultsList.length === 0 &&
           <p>Sorry, we couldn't find any cocktails that match your search.</p>
