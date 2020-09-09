9e2c0b62d0ed056f17388d535967cf227da4c639
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-09 16:54:56 -0600

    Create Results component structure and render

---
+++ src/components/Results/Results.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import DrinkCard from '../DrinkCard/DrinkCard';
+import './Results.css'
+import results from './testdata'
+
+const Results = () => {
+  const resultsList = results.map(result => {
+    return <DrinkCard />
+  })
+
+  return (
+      <section>
+        {resultsList.length > 0 &&
+          {resultsList}
+        }
+        {resultsList.length === 0 &&
+          <p>Sorry, we couldn't find any cocktails that match your search.</p>
+        }
+      </section>
+  )
+}
+
+export default Results
