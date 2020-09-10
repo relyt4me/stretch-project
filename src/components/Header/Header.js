ab86cb1ed05d05a1267a8434b4360fd86de84576
Tyler Haglund <relyt4me@gmail.com>
2020-09-09 18:48:07 -0600

    Add the logo to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import f2glogo from '../../assets/f2glogo.png';
 
 const Header = () => {
   return (
     <div className='header-component'>
       <h1 className='header-title'>Fridge to Glass</h1>
+      <img src={f2glogo} alt='Three neon glasses logo for Fridge to Glass' />
     </div>
   );
 };
