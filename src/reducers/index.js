00f36d0fd5cd50080b2133aebcf04af223ecc10b
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-10 12:21:31 -0600

    Add root reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import drinksListReducer from './drinksList-reducer';
+
+const rootReducer = combineReducers({
+  drinksListReducer
+})
+
+export default rootReducer 
