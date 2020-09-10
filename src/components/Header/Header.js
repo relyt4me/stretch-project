60a236feba0c34722a5fce073cbc30c8f6b630ff
Tyler Haglund <relyt4me@gmail.com>
2020-09-09 19:26:57 -0600

    Add alt to image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import f2glogo from '../../assets/f2glogo.png';
+import './Header.css';
 
 const Header = () => {
   return (
     <div className='header-component'>
       <h1 className='header-title'>Fridge to Glass</h1>
-      <img src={f2glogo} alt='Three neon glasses logo for Fridge to Glass' />
+      <img src={f2glogo} alt='Three neon glasses logo for Fridge to Glass' className='logo' />
     </div>
   );
 };
