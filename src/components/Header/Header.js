3ae60520d05b60d833180b1c6214a379f570ddce
Tyler Haglund <relyt4me@gmail.com>
2020-09-09 18:16:44 -0600

    Create the functional component Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header-component'>
+      <h1 className='header-title'>Fridge to Glass</h1>
+    </div>
+  );
+};
+
+export default Header;
