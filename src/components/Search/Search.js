e63322089857137c4d5eca1648b6ed79ddbd5f4e
Tyler Haglund <relyt4me@gmail.com>
2020-09-09 20:56:03 -0600

    Put parts of form in wrapper for styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -13,26 +13,32 @@ class Search extends Component {
   render() {
     return (
       <form className='search-component'>
-        <label htmlFor='searchPhrase' className='searchPhrase'>
-          What do you have?
-        </label>
-        <input id='searchPhrase' className='searchPhrase-input' type='text' />
-        <span id='rg-label'>Preference</span>
-        <div role='radiogroup' aria-labelledby='rg-label'>
-          <input type='radio' id='both' name='preference' value='both' checked />
-          <label for='both'>Both</label>
-          <input type='radio' id='alcoholic' name='preference' value='alcoholic' />
-          <label for='alcoholic'>Alcoholic</label>
-          <input type='radio' id='non-alcoholic' name='preference' value='non-alcoholic' />
-          <label for='non-alcoholic'>Non Alcoholic</label>
+        <div className='search-label-wrapper'>
+          <label htmlFor='searchPhrase' className='searchPhrase'>
+            What do you have?
+          </label>
+          <input id='searchPhrase' className='searchPhrase-input' type='text' placeholder='Vodka' />
+        </div>
+        <div className='search-preference-wrapper'>
+          <span id='rg-label'>Preference</span>
+          <div role='radiogroup' aria-labelledby='rg-label' className='preference-radio-wrapper'>
+            <input type='radio' id='both' name='preference' value='both' checked />
+            <label for='both'>Both</label>
+            <input type='radio' id='alcoholic' name='preference' value='alcoholic' />
+            <label for='alcoholic'>Alcoholic</label>
+            <input type='radio' id='non-alcoholic' name='preference' value='non-alcoholic' />
+            <label for='non-alcoholic'>Non Alcoholic</label>
+          </div>
         </div>
         {this.state.error && <label className='invalid-alert hide'>{this.state.error}</label>}
-        <button className='search' aria-label='Submit'>
-          Find Drinks
-        </button>
-        <button className='random' aria-label='Random'>
-          Random Drink
-        </button>
+        <div className='search-buttons-wrapper'>
+          <button className='search' aria-label='Submit'>
+            Find Drinks
+          </button>
+          <button className='random' aria-label='Random'>
+            Random Drink
+          </button>
+        </div>
       </form>
     );
   }
