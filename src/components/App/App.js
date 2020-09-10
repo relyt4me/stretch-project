b4bff3db513daf613d3f2df62288e7fdca78165c
Tyler Haglund <relyt4me@gmail.com>
2020-09-10 09:39:42 -0600

    Render Search and Header in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
+import Search from '../Search/Search';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,12 +6,21 @@ class Search extends Component {
     super(props);
     this.state = {
       searchPhrase: '',
+      preference: '',
       error: '',
     };
   }
 
   handleChange = (event) => {
     this.setState({ searchPhrase: event.target.value });
+  };
+
+  searchClick = (event) => {
+    event.preventDefault();
+  };
+
+  randomClick = (event) => {
+    event.preventDefault();
   };
 
   render() {
@@ -26,20 +35,26 @@ class Search extends Component {
         <div className='search-preference-wrapper'>
           <span id='rg-label'>Preference</span>
           <div role='radiogroup' aria-labelledby='rg-label' className='preference-radio-wrapper'>
-            <input type='radio' id='both' name='preference' value='both' checked />
-            <label for='both'>Both</label>
-            <input type='radio' id='alcoholic' name='preference' value='alcoholic' />
-            <label for='alcoholic'>Alcoholic</label>
-            <input type='radio' id='non-alcoholic' name='preference' value='non-alcoholic' />
-            <label for='non-alcoholic'>Non Alcoholic</label>
+            <label for='both'>
+              <input type='radio' id='both' name='preference' value='both' />
+              Both
+            </label>
+            <label for='alcoholic'>
+              <input type='radio' id='alcoholic' name='preference' value='alcoholic' />
+              Alcoholic
+            </label>
+            <label for='non-alcoholic'>
+              <input type='radio' id='non-alcoholic' name='preference' value='non-alcoholic' />
+              Non Alcoholic
+            </label>
           </div>
         </div>
         {this.state.error && <label className='invalid-search'>{this.state.error}</label>}
         <div className='search-buttons-wrapper'>
-          <button className='search' aria-label='Submit'>
+          <button className='search' aria-label='Submit' onClick={this.searchClick}>
             Find Drinks
           </button>
-          <button className='random' aria-label='Random'>
+          <button className='random' aria-label='Random' onClick={this.randomClick}>
             Random Drink
           </button>
         </div>
