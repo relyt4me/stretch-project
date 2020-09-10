0f648992018db0009ce7c826b76fec0438603542
Tyler Haglund <relyt4me@gmail.com>
2020-09-10 12:01:24 -0600

    Create test for changing of radio buttons

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Search from './Search';
 
 describe('Search', () => {
@@ -10,10 +10,55 @@ describe('Search', () => {
     const searchBox = screen.getByPlaceholderText('Vodka');
     const preferenceLabel = screen.getByText('Preference');
     const preferenceBoth = screen.getByRole('radio', { name: 'Both' });
+    const preferenceAlcoholic = screen.getByRole('radio', { name: 'Alcoholic' });
+    const preferenceNonAlcoholic = screen.getByRole('radio', { name: 'Non Alcoholic' });
+    const findButton = screen.getByRole('button', { name: 'Find' });
+    const randomButton = screen.getByRole('button', { name: 'Random' });
 
     expect(searchLabel).toBeInTheDocument();
     expect(searchBox).toBeInTheDocument();
     expect(preferenceLabel).toBeInTheDocument();
     expect(preferenceBoth).toBeInTheDocument();
+    expect(preferenceAlcoholic).toBeInTheDocument();
+    expect(preferenceNonAlcoholic).toBeInTheDocument();
+    expect(findButton).toBeInTheDocument();
+    expect(randomButton).toBeInTheDocument();
   });
+
+  it('Should change change the input as the form is filled out', () => {
+    render(<Search />);
+
+    const searchBox = screen.getByPlaceholderText('Vodka');
+
+    fireEvent.change(searchBox, { target: { value: 'Gin' } });
+
+    expect(searchBox.value).toBe('Gin');
+  });
+
+  it('Should check the radio buttons as the form is filled out', () => {
+    render(<Search />);
+
+    const preferenceBoth = screen.getByRole('radio', { name: 'Both' });
+    const preferenceAlcoholic = screen.getByRole('radio', { name: 'Alcoholic' });
+    const preferenceNonAlcoholic = screen.getByRole('radio', { name: 'Non Alcoholic' });
+
+    fireEvent.click(preferenceBoth);
+
+    expect(preferenceBoth).toBeChecked();
+
+    fireEvent.click(preferenceAlcoholic);
+
+    expect(preferenceBoth).not.toBeChecked();
+    expect(preferenceAlcoholic).toBeChecked();
+
+    fireEvent.click(preferenceNonAlcoholic);
+
+    expect(preferenceBoth).not.toBeChecked();
+    expect(preferenceAlcoholic).not.toBeChecked();
+    expect(preferenceNonAlcoholic).toBeChecked();
+  });
+
+  it('Should fire the searchClick method on click of search button', () => {});
+
+  it('Should fire the searchClick method on click of search button', () => {});
 });
