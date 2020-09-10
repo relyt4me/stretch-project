9ac40fad3d127c8a8738109be675731d6cef3f9d
Taryn Martin <taryn.martin919@gmail.com>
2020-09-09 20:11:54 -0600

    Change how data is passed into DrinkRecipe by passing through where component is called

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Results from '../Results/Results';
 import DrinkRecipe from '../DrinkRecipe/DrinkRecipe';
+import drinkData from './DrinkRecipe/drinkData.js';
 import { Route } from 'react-router-dom';
 
 //change /drinkRecipe to /:drinkId when we start importing data from api
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className='App'>
       <Route exact path='/drinkRecipe' render={() =>
-        <DrinkRecipe />
+        <DrinkRecipe drinkData={drinkData}/>
       } />
       <Route exact path='/' render={() =>
         <Results />
