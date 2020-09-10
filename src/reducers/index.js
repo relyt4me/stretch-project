fbff04b14c66c21f97f095c426f5b17c88d63b5f
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-10 15:13:31 -0600

    Add new reducers to combiner

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux'
 import drinksListReducer from './drinksList-reducer';
+import alcoholicDrinksReducer from './alcoholicDrinks-reducer'
+import nonAlcoholicDrinksReducer from './nonAlcholicDrinks-reducer'
 
 const rootReducer = combineReducers({
-  drinksListReducer
+  drinksListReducer,
+  nonAlcoholicDrinksReducer,
+  alcoholicDrinksReducer
 })
 
 export default rootReducer 
