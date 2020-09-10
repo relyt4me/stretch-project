6aea87b6163329a7e3e63d776ae79ecdd590f8bc
Taryn Martin <taryn.martin919@gmail.com>
2020-09-09 19:38:05 -0600

    Add react router and path to DrinkRecipe component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+import Results from '../Results/Results';
+import DrinkRecipe from '../DrinkRecipe/DrinkRecipe';
+import { Route } from 'react-router-dom';
+
+//change /drinkRecipe to /:drinkId when we start importing data from api
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Route exact path='/drinkRecipe' render={() =>
+        <DrinkRecipe />
+      } />
+      <Route exact path='/' render={() =>
+        <Results />
+      } />
+    </div>
+  )
 }
 
 export default App;
