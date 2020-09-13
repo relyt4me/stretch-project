a55df74679fbe328489cc583a4d68d202fa24155
Tyler Haglund <relyt4me@gmail.com>
2020-09-13 15:18:23 -0600

    Add provider and api call imports to search test

--- src/components/Search/Search.test.js
+++ src/components/Search/Search.test.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../../reducers/index';
 import Search from './Search';
+import { fetchDrinkByIngredient, fetchRandomDrink } from '../../helpers/apiCalls';
+import '@testing-library/jest-dom';
+jest.mock('../../helpers/apiCalls');
 
-describe('Search', () => {
+describe.only('Search', () => {
+  let store;
+  beforeEach(() => {
+    store = createStore(rootReducer);
+  });
   it('Should display search form on render', () => {
-    render(<Search />);
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Search />
+        </MemoryRouter>
+      </Provider>
+    );
 
     const searchLabel = screen.getByText('What do you have?');
     const searchBox = screen.getByPlaceholderText('Vodka');
@@ -27,7 +44,13 @@ describe('Search', () => {
   });
 
   it('Should change change the input as the form is filled out', () => {
-    render(<Search />);
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Search />
+        </MemoryRouter>
+      </Provider>
+    );
 
     const searchBox = screen.getByPlaceholderText('Vodka');
 
@@ -37,7 +60,13 @@ describe('Search', () => {
   });
 
   it('Should check the radio buttons as the form is filled out', () => {
-    render(<Search />);
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <Search />
+        </MemoryRouter>
+      </Provider>
+    );
 
     const preferenceBoth = screen.getByRole('radio', { name: 'Both' });
     const preferenceAlcoholic = screen.getByRole('radio', { name: 'Alcoholic' });
@@ -59,15 +88,39 @@ describe('Search', () => {
     expect(preferenceNonAlcoholic).toBeChecked();
   });
 
-  it('Should fire the searchClick method on click of search button', () => {
+  it('Should filter the search and call handle Search when inputs are filled out for a alcohol preference', () => {
     // render(<Search />);
     // const findButton = screen.getByRole('button', { name: 'Find' });
     // fireEvent.click(findButton);
   });
 
-  it('Should fire the searchClick method on click of search button', () => {
+  it('Should call handle Search when inputs are filled out without an alcohol preference', () => {
+    // render(<Search />);
+    // const randomButton = screen.getByRole('button', { name: 'Random' });
+    // fireEvent.click(randomButton);
+  });
+
+  it('Should call handleError when there is no valid search', () => {
+    // render(<Search />);
+    // const randomButton = screen.getByRole('button', { name: 'Random' });
+    // fireEvent.click(randomButton);
+  });
+
+  it('Should call handleError when there is a bad response from the server', () => {
+    // render(<Search />);
+    // const randomButton = screen.getByRole('button', { name: 'Random' });
+    // fireEvent.click(randomButton);
+  });
+
+  it('Should return only the necessary information from the redux store', () => {
     // render(<Search />);
     // const randomButton = screen.getByRole('button', { name: 'Random' });
     // fireEvent.click(randomButton);
   });
 });
+
+// test line 35
+// line 37
+// line 41
+// line 67
+// test props based on message
