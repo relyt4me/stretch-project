f09c9be01e28510d93a5a5713b0c8b2a229caa44
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-14 19:45:16 -0600

    Fix drinksList reducer test

--- src/reducers/drinksList-reducer.test.js
+++ src/reducers/drinksList-reducer.test.js
@@ -2,20 +2,14 @@ import drinksListReducer from './drinksList-reducer'
 
 describe('drinksListReducer', () => {
   it('should return the initial state', () => {
-    const expected = [];
-    const result = drinksListReducer(undefined, {});
+    const expected = null;
+    const result = drinksListReducer(null, {});
 
     expect(result).toEqual(expected)
   });
 
   it('should return the correct state if action is ADD_DRINKS_LIST', () => {
-    const initialState = [
-      {
-        strDrink: 'G & T',
-        strDrinkThumb: "https://g-and-t-img.com",
-        idDrink: '2'
-      }
-    ];
+    const initialState = null 
     const action = {
       type: 'ADD_DRINKS_LIST',
       drinksList: [
@@ -28,11 +22,6 @@ describe('drinksListReducer', () => {
     }
     const newState = [
       {
-        strDrink: 'G & T',
-        strDrinkThumb: "https://g-and-t-img.com",
-        idDrink: '2'
-      },
-      {
         strDrink: 'Toms Margarita',
         strDrinkThumb: "https://margarita-img.com",
         idDrink: '1'
@@ -42,5 +31,26 @@ describe('drinksListReducer', () => {
     const result = drinksListReducer(initialState, action);
 
     expect(result).toEqual(newState);
+  });
+
+  it('should return the correct state if action is RESET_DRINKS_LIST ', () => {
+    const initialState = [
+      {
+        strDrink: 'G & T',
+        strDrinkThumb: "https://g-and-t-img.com",
+        idDrink: '2'
+      }
+    ];
+
+    const action = {
+      type: 'RESET_DRINKS_LIST',
+      drinksList: []
+    }
+
+    const newState = []
+
+    const result = drinksListReducer(initialState, action);
+
+    expect(result).toEqual(newState)
   })
 })
