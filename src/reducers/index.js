dad3573a5bec80e06c49993d8ae0c013c4a532d9
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-14 20:00:25 -0600

    Change non-alcoholic tag color

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,6 @@ import nonAlcoholicDrinksReducer from './nonAlcoholicDrinks-reducer';
 import errorReducer from './error-reducer';
 import drinkRecipeReducer from './drinkRecipe-reducer';
 import drinkIdReducer from './drinkId-reducer';
-import isLoadingReducer from './isLoading-reducer'
 
 const rootReducer = combineReducers({
   drinkRecipe: drinkRecipeReducer,
@@ -13,8 +12,7 @@ const rootReducer = combineReducers({
   drinksList: drinksListReducer,
   nonAlcoholicDrinks: nonAlcoholicDrinksReducer,
   alcoholicDrinks: alcoholicDrinksReducer,
-  errorMessage: errorReducer,
-  isLoading: isLoadingReducer
+  errorMessage: errorReducer
 });
 
 export default rootReducer;
